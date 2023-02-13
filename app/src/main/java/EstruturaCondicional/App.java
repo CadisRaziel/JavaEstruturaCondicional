69fbe1d7bfdbc0207de82c799f0a5c1e24942e17
vitor <vitorcsharp@hotmail.com>
2023-02-13 18:00:20 -0300

    estrutura condicional

---
+++ app/src/main/java/EstruturaCondicional/App.java
@@ -0,0 +1,17 @@
+package EstruturaCondicional;
+
+public class App {
+
+    public static void main(String[] args) {
+        int media = 9;
+       if (media >= 7) {
+            if (media == 10) {
+                System.out.println("parabens voce foi aprovado com nota 10");
+            } else {
+                System.out.println("Parabens voce foi aprovado.");
+            }
+        } else {
+           System.out.println("voce nao passou");
+       }
+    }
+}
